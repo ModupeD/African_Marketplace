8ef88643a69104fdf18a575eb0dda2a6e1a57e19
Arison13 <arisonarias@gmail.com>
2021-08-24 15:13:22 -0400

    finished the styling for form.js

--- african-marketplace/src/components/form.js
+++ african-marketplace/src/components/form.js
@@ -1,50 +1,47 @@
 import React from 'react';
+import './form.css'
 
 export default function Form(){
 
 //the js so that the add items form can communicate with frontend and the backend, and that the buttons can work should be here
     return (
-       //need the nav bar on top. Adam is working on that
+       //need the nav bar on top. 
        <div className="form-container">
+          
            <h1>Add Items</h1>
-        <form id="add-items">  
-            <div className="item-name">
-                <label>
-                    Name of Item: 
-                    <input id='item' type='text' name='create-item' placeholder='Name of Item'></input>
-                </label>
-            </div>
+
+        <form> 
+            <h2> Item Information</h2>
+            <div className="main-container">
                 
-            <div> 
-            <label>
-                    Price of Item:
+                <div className="item-name">
+                    <input id='item' type='text' name='create-item' placeholder='Name of Item'></input>
+                </div>
+                    
+                <div className="item-price"> 
                     <input id='price' type='text' name='item-price' placeholder='Price of Item'></input>
-                </label>
-            </div>
+                </div>
 
-            <div className="item-name">
-                <label>
-                    Description: 
+                <div className="item-name">
                     <input id='description' type='text' name='item-description' placeholder='Add description'></input>
-                </label>
-            </div>
+                </div>
 
-            <div className="item-name">
-                <select name="location" id="location-dropdown"> 
-                    <option> Select Location</option>
-                    <option> COUNTRIES FROM AFRICA</option> 
-                    {/* i dont know the countries in africa so let me know and ill change this */}
-                </select>
-            </div>
+                <div className="item-name">
+                    <select className="location" id="location-dropdown"> 
+                        <option> Select Location</option>
+                        <option> COUNTRIES FROM AFRICA</option> 
+                        {/* i dont know the countries in africa so let me know and ill change this */}
+                    </select>
+                </div>
 
-            <div className="submit-img"> 
-                <label>
-                    Add image (Optional):
-                    <input id='img' type='file' name='img'></input>
-                </label>
+                <div className="img"> 
+                    <label>
+                        Add image (Optional):
+                        <input id='img' type='file' name='img'></input>
+                    </label>
+                </div>
+                <button className="submit-btn"> Publish </button>
             </div>
-            <button className="submit-btn"> Publish </button>
-            
 
 
         </form>
