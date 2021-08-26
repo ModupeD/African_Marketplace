338149d80247d8f177b0fd55fadee37735213074
ModupeD <bimisoladaniel@Bimisolas-MacBook-Air.local[credential]>
2021-08-26 17:03:49 +0100

    made progress

--- african-marketplace/src/App.js
+++ african-marketplace/src/App.js
@@ -1,10 +1,28 @@
 import "./App.css";
+
+import ReactDOM from "react-dom";
 import Form from "./components/form";
+import Home from "./components/Home";
+import {
+  Switch,
+  Route,
+  Redirect,
+  BrowserRouter as Router,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Form Form={Form} />
+          </Route>
+          <Route path="/home">
+            <Home Home={Home} />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- african-marketplace/src/components/Home.js
+++ african-marketplace/src/components/Home.js
@@ -1,5 +1,45 @@
 import React from "react";
+import "./Home.css";
 
-export default function Home() {
-  return <div>I am at home</div>;
+function Home() {
+  return (
+    <div>
+      <div class="source">
+        <div class="headingstyle">
+          <header>
+            <title>Site Title</title>
+            <h2>African Market </h2>
+            <nav>
+              <button>
+                <a href="/home">Home</a>
+              </button>
+              <button>
+                <a href="profile">Profile</a>
+              </button>
+              <button>
+                <a href="/">Add Items</a>
+              </button>
+            </nav>
+          </header>
+        </div>
+      </div>
+      <body>
+        <section>
+          <div class="organize">
+            <div class="boxOne"></div>
+            <div class="boxTwo"></div>
+            <div class="boxThree"></div>
+          </div>
+
+          <div class="reorganize">
+            <div class="boxFour"></div>
+            <div class="boxFive"></div>
+            <div class="boxSix"></div>
+          </div>
+        </section>
+      </body>
+    </div>
+  );
 }
+
+export default Home;
